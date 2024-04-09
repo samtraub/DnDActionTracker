9a110e4e0408278f223faa5ac85ac9988d9fc84d
Sam Traub <sat.asher@gmail.com>
2024-04-09 16:57:33 -0400

    Dropdown populated with existing characters

--- DnDActionTracker/MainModule.vb
+++ DnDActionTracker/MainModule.vb
@@ -4,6 +4,15 @@ Imports Newtonsoft.Json
 Module MainModule
     Private CharacterPath As String = Path.Combine(Application.StartupPath, "Characters")
 
+    Public Function GetCharacterList() As String()
+        Dim charList As New List(Of String)
+        For Each file As String In Directory.GetFiles(CharacterPath, "*.json")
+            charList.Add(Path.GetFileNameWithoutExtension(file))
+        Next
+
+        Return charList.ToArray
+    End Function
+
     Public Sub SaveCharacter(ByVal name As String, ByVal actions As List(Of Action))
         Dim character As New Character(name, actions)
         If Not Directory.Exists(CharacterPath) Then
